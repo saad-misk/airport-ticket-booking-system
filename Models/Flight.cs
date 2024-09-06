d07675f43a83dce8ddedf8c859fff6250a4dbd18
Saad Misk <221191@ppu.edu.ps>
2024-09-07 00:46:28 +0300

    Add Flight class with properties for flight details

---
+++ Models/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Models
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string DepartureCountry { get; set; }
+        public string DestinationCountry { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public Dictionary<string, decimal> ClassPrices { get; set; } // Key: Class Name, Value: Price
+
+    }
+}
