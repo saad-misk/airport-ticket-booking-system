e433a8fc04770031141800718eca5e4f61e17181
Saad Misk <221191@ppu.edu.ps>
2024-09-07 00:47:10 +0300

    Add Booking class with properties for Booking details

---
+++ Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Models
+{
+    public class Booking
+    {
+        public string BookingId { get; set; }
+        public string FlightNumber { get; set; }
+        public string PassengerId { get; set; }
+        public string Class { get; set; }
+        public DateTime BookingDate { get; set; }
+
+    }
+}
