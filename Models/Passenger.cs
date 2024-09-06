0261382f76efc422dfc2e8bbf673c569c05d3368
Saad Misk <221191@ppu.edu.ps>
2024-09-07 00:48:51 +0300

    Add Passenger class with properties for Passenger details

---
+++ Models/Passenger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Models
+{
+    public class Passenger
+    {
+        public string PassengerId {  get; set; }
+        public string Name { get; set; }
+        public List<Booking> Bookings { get; set; }
+
+    }
+}
