fdb0a699228fec3aac72bcea1901c9b874034cf0
Saad Misk <221191@ppu.edu.ps>
2024-09-08 19:59:31 +0300

    feat: Add menu strategy files for passenger and manager roles
    - Added IMenuStrategy interface to define common contract for menu strategies.
    - Implemented ManagerMenuStrategy for handling manager-specific menu actions.
    - Implemented PassengerMenuStrategy for handling passenger-specific menu actions.
    - Organized new classes into appropriate namespaces (Helpers and Views).

---
+++ Views/PassengerMenuStrategy.cs
@@ -0,0 +1,50 @@
+﻿using AirportTicketBookingSystem.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Views
+{
+    public class PassengerMenuStrategy : IMenuStrategy
+    {
+        private readonly CommandFactory _commandFactory;
+
+        public PassengerMenuStrategy(CommandFactory commandFactory)
+        {
+            _commandFactory = commandFactory;
+        }
+
+        public void DisplayMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Passenger Menu:");
+                Console.WriteLine("1. Book Flight");
+                Console.WriteLine("2. View Bookings");
+                Console.WriteLine("3. Cancel Booking");
+                Console.WriteLine("4. Modify Booking");
+                Console.WriteLine("5. Search for available flights");
+                Console.WriteLine("6. Logout");
+
+                var choice = Console.ReadLine() ?? "7";
+
+                if (choice == "6") return;
+
+                try
+                {
+                    var command = _commandFactory.CreatePassengerCommand(choice);
+                    command.Execute();
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
