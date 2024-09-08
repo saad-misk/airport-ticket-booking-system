58febfcc9d70ea29925a1a2e16811f270a6ca3a2
Saad Misk <221191@ppu.edu.ps>
2024-09-08 08:58:18 +0300

    Add FilterCriteria class to help with searching for a flight based on it's properties

---
+++ Services/FilterCriteria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Services
+{
+    public class FilterCriteria
+    {
+        public string FlightNumber { get; set; }
+        public decimal? Price { get; set; }
+        public string DepartureCountry { get; set; }
+        public string DestinationCountry { get; set; }
+        public DateTime? DepartureDate { get; set; } // Nullable DateTime
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public string Passenger { get; set; }
+        public string Class { get; set; }
+    }
+}
