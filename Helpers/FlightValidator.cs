368e13a8118737585ead0c37ea6739b6f006daaf
Saad Misk <221191@ppu.edu.ps>
2024-09-08 12:16:38 +0300

    Add FlightValidator class in helper folder that checks the validaty of a list of flights

---
+++ Helpers/FlightValidator.cs
@@ -0,0 +1,34 @@
+﻿using AirportTicketBookingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Helpers
+{
+    public class FlightValidator
+    {
+        public List<string> ValidateFlight(List<Flight> flights)
+        {
+            var errors = new List<string>();
+
+            foreach (var flight in flights)
+            {
+                var validationResults = new List<ValidationResult>();
+                var context = new ValidationContext(flight);
+
+                if( !Validator.TryValidateObject(flight, context, validationResults))
+                {
+                    foreach (var validationResult in validationResults)
+                    {
+                        errors.Add($"Flight {flight.FlightNumber}: {validationResult.ErrorMessage}");
+                    }
+                }
+            }
+
+            return errors;
+        }
+    }
+}
