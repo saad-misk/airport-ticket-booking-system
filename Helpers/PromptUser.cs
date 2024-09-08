961f6b4d3a3dadc300c90702a98ca246b2d6b1f7
Saad Misk <221191@ppu.edu.ps>
2024-09-08 20:36:22 +0300

    Add Helpers/PromptUser.cs to read data from user

---
+++ Helpers/PromptUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Helpers
+{
+    public class PromptUser
+    {
+        public string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine() ?? "";
+        }
+
+        public DateTime PromptForDate(string prompt)
+        {
+            DateTime date;
+            while (true)
+            {
+                Console.Write(prompt);
+                if (DateTime.TryParse(Console.ReadLine(), out date))
+                {
+                    return date;
+                }
+                Console.WriteLine("Invalid date format. Please try again.");
+            }
+        }
+
+        public decimal PromptForDecimal(string prompt)
+        {
+            decimal number;
+            while (true)
+            {
+                Console.Write(prompt);
+                if (decimal.TryParse(Console.ReadLine(), out number))
+                {
+                    return number;
+                }
+                Console.WriteLine("Invalid number format. Please try again.");
+            }
+        }
+    }
+}
