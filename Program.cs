c323bd611f12f3ac33ad593504711c2279c2d580
Saad Misk <221191@ppu.edu.ps>
2024-09-08 20:02:38 +0300

    Update Program.cs to integrate new menu strategies, Include role-based menu handling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AirportTicketBookingSystem.Helpers;
+using AirportTicketBookingSystem.Services;
+using AirportTicketBookingSystem.Views;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var bookingService = new BookingService();
+        var commandFactory = new CommandFactory(bookingService);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Select Role:");
+            Console.WriteLine("1. Passenger");
+            Console.WriteLine("2. Manager");
+            Console.WriteLine("3. Exit");
+
+            var roleChoice = Console.ReadLine();
+            IMenuStrategy menuStrategy;
+
+            switch (roleChoice)
+            {
+                case "1":
+                    menuStrategy = new PassengerMenuStrategy(commandFactory);
+                    break;
+                case "2":
+                    menuStrategy = new ManagerMenuStrategy(commandFactory);
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    continue;
+            }
+
+            menuStrategy.DisplayMenu();
+        }        
+
+    }
+}
